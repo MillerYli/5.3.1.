5d4d368655fb16be1dec0870b8f163fef64e2751
Yulia Miller <miller82.98@mail.ru>
2022-12-28 23:22:21 -0800

    Добавьте файлы проекта.

---
+++ 5.3.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+            int myage = 24;
+            Console.WriteLine("Мой возраст: " + myage);
+
+            ChangeAge(myage);
+
+            Console.WriteLine("Мой измененный возраст: " + myage);
+
+        }
+
+        static void ChangeAge(int age)
+        {
+            Console.Write("Введите свой возраст: ");
+
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
